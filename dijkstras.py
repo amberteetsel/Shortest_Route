db6de29b0b01a4f8ce6b13f913ea568a0a7aa3b5
Soorej S Nair <soorejsnair@Soorejs-MacBook-Air.local>
2025-11-18 12:06:38 -0700

    Added peak and avg traffic scenarios

--- dijkstras.py
+++ dijkstras.py
@@ -2,35 +2,46 @@ from src.modules.graph import Graph
 import json
 
 
-with open('locations.json', 'r') as f:
+with open('manhattan_driving_distances.json', 'r') as f:
     loaded_dict = json.load(f)
 
-g = Graph(len(loaded_dict))
+# average traffic time
+g = Graph(len(loaded_dict['nodes']))
 
-i = 0
-for key in loaded_dict.keys():
-    g.add_vertex_data(i, key)
-    i += 1
+for key in loaded_dict['nodes'].keys():
+    g.add_vertex_data(int(key), loaded_dict['nodes'][key]["name"])
 
-g.add_edge(0, 1, 9)
-g.add_edge(0, 2, 3)
-g.add_edge(0, 3, 3)
-g.add_edge(1, 7, 2)
-g.add_edge(2, 3, 2)
-g.add_edge(2, 4, 6)
-g.add_edge(2, 6, 2)
-g.add_edge(3, 4, 3)
-g.add_edge(6, 7, 1)
-g.add_edge(6, 5, 3)
+for i in loaded_dict["pairwise"].keys():
+    for j in loaded_dict["pairwise"][i].keys():
+        g.add_edge(int(i), int(j), loaded_dict["pairwise"][i][j]['avg_time_min'])
 
-
-distances = g.dijkstra('Times Square (Broadway & 7th Ave)')
+distances = g.dijkstra("Times Square")
 for i, d in enumerate(distances):
-    print(f"Distance from Times Square (Broadway & 7th Ave) to {g.vertex_data[i]}: {d}")
-distances = g.dijkstra('Empire State Building (350 5th Ave)')
+    print(f"Distance from Times Square to {g.vertex_data[i]} during average traffic hours: {d}")
+distances = g.dijkstra("Rockefeller Center")
 for i, d in enumerate(distances):
-    print(f"Distance from Empire State Building (350 5th Ave) to {g.vertex_data[i]}: {d}")
-distances = g.dijkstra('Union Square (14th St & Broadway)')
+    print(f"Distance from Empire State Building to {g.vertex_data[i]} during average traffic hours: {d}")
+distances = g.dijkstra("Empire State Building")
 for i, d in enumerate(distances):
-    print(f"Distance from 'Union Square (14th St & Broadway)' to {g.vertex_data[i]}: {d}")
+    print(f"Distance from 'Union Square ' to {g.vertex_data[i]} during average traffic hours : {d}")
+
+# peak traffic time
+g = Graph(len(loaded_dict['nodes']))
+
+for key in loaded_dict['nodes'].keys():
+    g.add_vertex_data(int(key), loaded_dict['nodes'][key]["name"])
+
+for i in loaded_dict["pairwise"].keys():
+    for j in loaded_dict["pairwise"][i].keys():
+        g.add_edge(int(i), int(j), loaded_dict["pairwise"][i][j]['peak_time_min'])
+
+distances = g.dijkstra("Times Square")
+for i, d in enumerate(distances):
+    print(f"Distance from Times Square to {g.vertex_data[i]} during peak traffic hours: {d}")
+distances = g.dijkstra("Rockefeller Center")
+for i, d in enumerate(distances):
+    print(f"Distance from Empire State Building to {g.vertex_data[i]} during peak traffic hours: {d}")
+distances = g.dijkstra("Empire State Building")
+for i, d in enumerate(distances):
+    print(f"Distance from 'Union Square ' to {g.vertex_data[i]} during peak traffic hours : {d}")
 
--- visualizer.py
+++ visualizer.py
@@ -1,14 +1,14 @@
 from gmplot import gmplot
 import json
 
-with open('locations.json', 'r') as f:
+with open('manhattan_driving_distances.json', 'r') as f:
     loc_dict = json.load(f)
 
 gmap = gmplot.GoogleMapPlotter(40.7570, -73.98597, 13)
 
-for key in loc_dict.keys():
-    print(key, loc_dict[key][0], loc_dict[key][1])
-    gmap.marker(loc_dict[key][0], loc_dict[key][1], 'cornflowerblue', title = key)
+for key in loc_dict['nodes'].keys():
+    print(key, loc_dict['nodes'][key]['name'], loc_dict['nodes'][key]['lat'], loc_dict['nodes'][key]['lon'])
+    gmap.marker(loc_dict['nodes'][key]['lat'], loc_dict['nodes'][key]['lon'], 'cornflowerblue', title = loc_dict['nodes'][key]['name'])
 
 gmap.draw("location_visualizer.html")
 
