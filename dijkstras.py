8222f5a189f87b20bf5e328609dc65a46b48c1cb
Soorej S Nair <soorejsnair@Soorejs-MacBook-Air.local>
2025-11-21 16:39:51 -0800

    Updated visualizer and test cases

--- dijkstras.py
+++ dijkstras.py
@@ -21,74 +21,93 @@ for i, d in enumerate(distances):
     print(f"Distance from Times Square to {g.vertex_data[i]} during average traffic hours: {d}")
 distances = g.dijkstra("Rockefeller Center")
 for i, d in enumerate(distances):
-    print(f"Distance from Empire State Building to {g.vertex_data[i]} during average traffic hours: {d}")
+    print(f"Distance from Rockefeller Center to {g.vertex_data[i]} during average traffic hours: {d}")
 distances = g.dijkstra("Empire State Building")
 for i, d in enumerate(distances):
-    print(f"Distance from 'Union Square ' to {g.vertex_data[i]} during average traffic hours : {d}")
+    print(f"Distance from Empire State Building to {g.vertex_data[i]} during average traffic hours : {d}")
+
+for i in g.adj_matrix:
+    for j in i:
+        print(j, '\t', end= '')
+    print()
+print()
+for i in g.adj_matrix:
+    for j in i:
+        print(j, '\t', end= '')
+    print()
 
 # Simulated peak traffic times
+g_sim_peak_traffic = Graph(len(loaded_dict['nodes']))
+for key in loaded_dict['nodes'].keys():
+    g_sim_peak_traffic.add_vertex_data(int(key), loaded_dict['nodes'][key]["name"])
+
 mean = 2
 std = 0.5
 for i in loaded_dict["pairwise"].keys():
     for j in loaded_dict["pairwise"][i].keys():
-        g.add_edge(int(i), int(j), loaded_dict["pairwise"][i][j]['avg_time_min']*np.random.normal(mean, std))
+        g_sim_peak_traffic.add_edge(int(i), int(j), loaded_dict["pairwise"][i][j]['avg_time_min']*np.random.normal(mean, std))
 
-# g.simulate_traffic()
-distances = g.dijkstra("Times Square")
+distances = g_sim_peak_traffic.dijkstra("Times Square")
 for i, d in enumerate(distances):
-    print(f"Distance from Times Square to {g.vertex_data[i]} during simulated peak traffic hours: {d}")
-distances = g.dijkstra("Rockefeller Center")
+    print(f"Distance from Times Square to {g_sim_peak_traffic.vertex_data[i]} during simulated peak traffic hours: {d}")
+distances = g_sim_peak_traffic.dijkstra("Rockefeller Center")
 for i, d in enumerate(distances):
-    print(f"Distance from Empire State Building to {g.vertex_data[i]} during simulated peak traffic hours: {d}")
-distances = g.dijkstra("Empire State Building")
+    print(f"Distance from Rockefeller Center to {g_sim_peak_traffic.vertex_data[i]} during simulated peak traffic hours: {d}")
+distances = g_sim_peak_traffic.dijkstra("Empire State Building")
 for i, d in enumerate(distances):
-    print(f"Distance from 'Union Square ' to {g.vertex_data[i]} during simulated peak traffic hours : {d}")
+    print(f"Distance from Empire State Building to {g_sim_peak_traffic.vertex_data[i]} during simulated peak traffic hours : {d}")
 
 # peak traffic time
-g = Graph(len(loaded_dict['nodes']))
+g_peak_traffic = Graph(len(loaded_dict['nodes']))
+for key in loaded_dict['nodes'].keys():
+    g_peak_traffic.add_vertex_data(int(key), loaded_dict['nodes'][key]["name"])
 
 for key in loaded_dict['nodes'].keys():
-    g.add_vertex_data(int(key), loaded_dict['nodes'][key]["name"])
+    g_peak_traffic.add_vertex_data(int(key), loaded_dict['nodes'][key]["name"])
 
 for i in loaded_dict["pairwise"].keys():
     for j in loaded_dict["pairwise"][i].keys():
-        g.add_edge(int(i), int(j), loaded_dict["pairwise"][i][j]['peak_time_min'])
+        g_peak_traffic.add_edge(int(i), int(j), loaded_dict["pairwise"][i][j]['peak_time_min'])
 
-distances = g.dijkstra("Times Square")
+distances = g_peak_traffic.dijkstra("Times Square")
 for i, d in enumerate(distances):
-    print(f"Distance from Times Square to {g.vertex_data[i]} during peak traffic hours: {d}")
-distances = g.dijkstra("Rockefeller Center")
+    print(f"Distance from Times Square to {g_peak_traffic.vertex_data[i]} during peak traffic hours: {d}")
+distances = g_peak_traffic.dijkstra("Rockefeller Center")
 for i, d in enumerate(distances):
-    print(f"Distance from Empire State Building to {g.vertex_data[i]} during peak traffic hours: {d}")
-distances = g.dijkstra("Empire State Building")
+    print(f"Distance from Rockefeller Center to {g_peak_traffic.vertex_data[i]} during peak traffic hours: {d}")
+distances = g_peak_traffic.dijkstra("Empire State Building")
 for i, d in enumerate(distances):
-    print(f"Distance from 'Union Square ' to {g.vertex_data[i]} during peak traffic hours : {d}")
+    print(f"Distance from Empire State Building to {g_peak_traffic.vertex_data[i]} during peak traffic hours : {d}")
 
 # Simulated disruptions in traffic
+g_sim_disruptions = Graph(len(loaded_dict['nodes']))
+for key in loaded_dict['nodes'].keys():
+    g_sim_disruptions.add_vertex_data(int(key), loaded_dict['nodes'][key]["name"])
+
 for i in loaded_dict["pairwise"].keys():
     for j in loaded_dict["pairwise"][i].keys():
         rand_val = random.random() 
         if rand_val < 0.8:
             # No disruption
-            g.add_edge(int(i), int(j), loaded_dict["pairwise"][i][j]['avg_time_min'])
+            g_sim_disruptions.add_edge(int(i), int(j), loaded_dict["pairwise"][i][j]['avg_time_min'])
         else:
             # Add disruption
             rand_val2 = random.random()
             if rand_val2 <= 0.5:
                 # By making weight for the edge 0
-                g.add_edge(int(i), int(j), 0)
+                g_sim_disruptions.add_edge(int(i), int(j), float('inf'))
             else:
                 # By making weight for the edge weight 10x
-                g.add_edge(int(i), int(j), loaded_dict["pairwise"][i][j]['avg_time_min']*10)
+                g_sim_disruptions.add_edge(int(i), int(j), loaded_dict["pairwise"][i][j]['avg_time_min']*10)
                 
 
-# g.simulate_traffic()
-distances = g.dijkstra("Times Square")
+# g_sim_disruptions.simulate_traffic()
+distances = g_sim_disruptions.dijkstra("Times Square")
 for i, d in enumerate(distances):
-    print(f"Distance from Times Square to {g.vertex_data[i]} with simulated disruption in traffic hours: {d}")
-distances = g.dijkstra("Rockefeller Center")
+    print(f"Distance from Times Square to {g_sim_disruptions.vertex_data[i]} with simulated disruption in traffic hours: {d}")
+distances = g_sim_disruptions.dijkstra("Rockefeller Center")
 for i, d in enumerate(distances):
-    print(f"Distance from Empire State Building to {g.vertex_data[i]} with simulated disruption in traffic hours: {d}")
-distances = g.dijkstra("Empire State Building")
+    print(f"Distance from Rockefeller Center to {g_sim_disruptions.vertex_data[i]} with simulated disruption in traffic hours: {d}")
+distances = g_sim_disruptions.dijkstra("Empire State Building")
 for i, d in enumerate(distances):
-    print(f"Distance from 'Union Square ' to {g.vertex_data[i]} with simulated disruption in traffic hours : {d}")
+    print(f"Distance from Empire State Building to {g_sim_disruptions.vertex_data[i]} with simulated disruption in traffic hours : {d}")
--- visualizer.py
+++ visualizer.py
@@ -1,13 +1,108 @@
-from gmplot import gmplot
 import json
+import folium
+from folium import plugins
 
 with open('manhattan_driving_distances.json', 'r') as f:
-    loc_dict = json.load(f)
+    graph_data = json.load(f)
 
-gmap = gmplot.GoogleMapPlotter(40.7570, -73.98597, 13)
+m = folium.Map(
+    location=[40.7425, -73.9875],
+    zoom_start=13,
+    tiles='OpenStreetMap'
+)
 
-for key in loc_dict['nodes'].keys():
-    print(key, loc_dict['nodes'][key]['name'], loc_dict['nodes'][key]['lat'], loc_dict['nodes'][key]['lon'])
-    gmap.marker(loc_dict['nodes'][key]['lat'], loc_dict['nodes'][key]['lon'], 'cornflowerblue', title = loc_dict['nodes'][key]['name'])
+drawn_edges = set()
 
-gmap.draw("location_visualizer.html")
+for from_id, connections in graph_data["pairwise"].items():
+    from_node = graph_data["nodes"][from_id]
+    
+    for to_id, edge_data in connections.items():
+        edge_key = tuple(sorted([from_id, to_id]))
+        
+        if edge_key not in drawn_edges:
+            to_node = graph_data["nodes"][to_id]
+            
+            distance = edge_data.get("avg_distance_km", 0)
+            avg_time = edge_data.get("avg_time_min", 0)
+            peak_time = edge_data.get("peak_time_min", 0)
+            
+            mid_lat = (from_node["lat"] + to_node["lat"]) / 2
+            mid_lon = (from_node["lon"] + to_node["lon"]) / 2
+            
+            edge_tooltip = f"""
+            {from_node['name']} ↔ {to_node['name']}<br>
+            Distance: {distance} km<br>
+            Avg Time: {avg_time} min<br>
+            Peak Time: {peak_time} min
+            """
+            
+            folium.PolyLine(
+                locations=[
+                    [from_node["lat"], from_node["lon"]],
+                    [to_node["lat"], to_node["lon"]]
+                ],
+                color='blue',
+                weight=3,
+                opacity=0.6,
+                tooltip=edge_tooltip
+            ).add_to(m)
+            
+            weight_label = f"{distance} km / {avg_time} min"
+            folium.Marker(
+                location=[mid_lat, mid_lon],
+                icon=folium.DivIcon(html=f"""
+                    <div style="font-size: 10px; color: #1a1a1a; 
+                                background-color: rgba(255, 255, 255, 0.8); 
+                                padding: 2px 4px; border-radius: 3px;
+                                border: 1px solid #0066cc;
+                                font-weight: bold; white-space: nowrap;">
+                        {weight_label}
+                    </div>
+                """),
+                tooltip=edge_tooltip
+            ).add_to(m)
+            
+            drawn_edges.add(edge_key)
+
+for node_id, node_data in graph_data["nodes"].items():
+    connections_count = len(graph_data["pairwise"].get(node_id, {}))
+    
+    popup_text = f"""
+    <b>{node_data['name']}</b><br>
+    Node ID: {node_id}<br>
+    Connections: {connections_count}<br>
+    Lat: {node_data['lat']:.5f}<br>
+    Lon: {node_data['lon']:.5f}
+    """
+    
+    folium.Marker(
+        location=[node_data["lat"], node_data["lon"]],
+        popup=folium.Popup(popup_text, max_width=250),
+        tooltip=node_data["name"],
+        icon=folium.Icon(color='red', icon='info-sign')
+    ).add_to(m)
+    
+    folium.CircleMarker(
+        location=[node_data["lat"], node_data["lon"]],
+        radius=8,
+        color='darkred',
+        fill=True,
+        fillColor='red',
+        fillOpacity=0.7,
+        weight=2
+    ).add_to(m)
+
+legend_html = '''
+<div style="position: fixed; 
+            bottom: 50px; right: 50px; width: 200px; height: 120px; 
+            background-color: white; border:2px solid grey; z-index:9999; 
+            font-size:14px; padding: 10px">
+<p><b>NYC Landmarks Graph</b></p>
+<p><span style="color:red;">●</span> Landmark Node</p>
+<p><span style="color:blue;">━</span> Direct Connection</p>
+<p>Total Nodes: 10<br>Total Edges: 13</p>
+</div>
+'''
+m.get_root().html.add_child(folium.Element(legend_html))
+
+m.save('connected_graph.html')
